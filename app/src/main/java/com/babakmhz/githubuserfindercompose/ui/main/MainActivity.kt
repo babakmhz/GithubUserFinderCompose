2b6d0b06a3f63c5dd1b91c57cc3da537d8e9394a
babakmhz <babakmhz96@gmail.com>
2022-02-19 01:12:23 +0330

    main_nav added

--- app/src/main/java/com/babakmhz/githubuserfindercompose/ui/main/MainActivity.kt
+++ app/src/main/java/com/babakmhz/githubuserfindercompose/ui/main/MainActivity.kt
@@ -3,36 +3,18 @@ package com.babakmhz.githubuserfindercompose.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.babakmhz.githubuserfindercompose.R
 import com.babakmhz.githubuserfindercompose.ui.theme.GithubUserFinderComposeTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GithubUserFinderComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GithubUserFinderComposeTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
