a9f1586941ac5da9bc26aeabecc459672f93581f
babakmhz <babakmhz96@gmail.com>
2022-02-19 01:06:17 +0330

    saerchBar compoenet implemented

---
+++ app/src/main/java/com/babakmhz/githubuserfindercompose/ui/components/SearchBar.kt
@@ -0,0 +1,88 @@
+package com.babakmhz.githubuserfindercompose.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+@ExperimentalComposeUiApi
+@Composable
+fun SearchBar(
+    query: MutableState<String>,
+    onQueryChanged: (StateFlow<String>) -> Unit,
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth(),
+        elevation = 8.dp,
+    ) {
+        Column(
+            modifier =
+            Modifier.fillMaxWidth()
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth(.9f)
+                        .padding(8.dp),
+                    value = query.value,
+                    onValueChange = {
+                        query.value = it
+                        val stateFlow = MutableStateFlow("")
+                        if (stateFlow.value != it) {
+                            stateFlow.value = it
+                        }
+                        onQueryChanged(stateFlow)
+                    },
+                    label = { Text(text = "Search") },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done,
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            keyboardController?.hideSoftwareKeyboard()
+                        },
+                    ),
+                    leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") },
+                    textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
+                    colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
+                )
+
+            }
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun previewSearchBar() {
+    val query = mutableStateOf("")
+    SearchBar(
+        query = query,
+        onQueryChanged = {},
+    )
+}
