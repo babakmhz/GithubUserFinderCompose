3426a379e7350ebd23c158c6473a489cebb226d6
babakmhz <babakmhz96@gmail.com>
2022-02-18 20:55:13 +0330

    utils package added

---
+++ app/src/main/java/com/babakmhz/githubuserfindercompose/utils/ExtensionFunction.kt
@@ -0,0 +1,24 @@
+package com.babakmhz.githubuserfindercompose.utils
+
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+
+fun String?.validString() = this != null && this.isNotEmpty()
+
+
+fun CoroutineScope.launchWithException(
+    livedata: MutableLiveData<Throwable?>,
+    loading: MutableLiveData<Boolean>,
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    return launch(CoroutineExceptionHandler { _, throwable ->
+        livedata.postValue(throwable)
+        loading.postValue(false)
+    }, block = block)
+}
+
+
