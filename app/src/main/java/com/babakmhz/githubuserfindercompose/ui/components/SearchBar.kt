c012ce8e44816cb3086c2f229e00a1bcea153bf1
babakmhz <babakmhz96@gmail.com>
2022-02-20 19:29:45 +0330

    searchbar icon and color changed

--- app/src/main/java/com/babakmhz/githubuserfindercompose/ui/components/SearchBar.kt
+++ app/src/main/java/com/babakmhz/githubuserfindercompose/ui/components/SearchBar.kt
@@ -1,28 +1,26 @@
 package com.babakmhz.githubuserfindercompose.ui.components
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import com.babakmhz.githubuserfindercompose.R
 
 
 @ExperimentalComposeUiApi
@@ -36,12 +34,16 @@ fun SearchBar(
         modifier = Modifier
             .fillMaxWidth(),
         elevation = 8.dp,
+        color = MaterialTheme.colors.primary
     ) {
         Column(
-            modifier =
-            Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
                 TextField(
                     modifier = Modifier
                         .fillMaxWidth(.9f)
@@ -51,7 +53,7 @@ fun SearchBar(
                         query.value = it
                         onQueryChanged(it)
                     },
-                    label = { Text(text = "Search") },
+                    label = { Text(text = stringResource(R.string.type_a_github_username)) },
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Text,
                         imeAction = ImeAction.Done,
@@ -61,7 +63,12 @@ fun SearchBar(
                             keyboardController?.hideSoftwareKeyboard()
                         },
                     ),
-                    leadingIcon = { Icon(Icons.Filled.Search, contentDescription = "Search Icon") },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.github_sign),
+                            contentDescription = "Search Icon"
+                        )
+                    },
                     textStyle = TextStyle(color = MaterialTheme.colors.onSurface),
                     colors = TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colors.surface),
                 )
